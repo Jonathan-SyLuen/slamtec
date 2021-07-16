2291a596a5d25605c065706fc8f4c77c5d371301
Jonathan Chang <jonathan.sy.luen@gmail.com>
2021-07-16 12:18:10 +0800

    Remove ignored files

--- disinfection_integration/android/app/src/main/kotlin/com/example/disinfection_integration/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.disinfection_integration
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- robot_clean/android/app/src/main/kotlin/com/example/robot_clean/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.robot_clean
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
